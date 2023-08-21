4181b3043e6b90477b2971dc8a01af67608d69dd
wetleaf <harsh1513088@gmail.com>
2023-08-21 16:01:30 +0530

    Sources

---
+++ route.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+traceroute -IAn google.com > google.txt
+traceroute -IAn alibaba.com > alibaba.txt
+traceroute -IAn wikipedia.org > wikipedia.txt
+traceroute -IAn bitcoin.org > bitcoin.txt
+traceroute -IAn amazon.com > amazon.txt
+traceroute -IAn spotify.com > spotify.txt
+traceroute -IAn ntt.com > ntt.txt
+traceroute -IAn vodacom.com > vodacom.txt
+traceroute -IAn apple.com > apple.txt
+traceroute -IAn goldmansachs.com > goldmansachs.txt
